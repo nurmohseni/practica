eeba61dd01188c91b03cded34a879438b7f60587
Nurullah Mohseni <Noorullahmohseni1@gmail.com>
2024-02-06 01:00:52 +0100

    Programa que ademas de imprimir Hola Mundo, Muestra fecha y hora actual

--- Principal.java
+++ Principal.java
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal{
     public static void main(String[] args){
         System.out.println("Hola Mundo");
+        System.out.println("Fecha y hora Actual:"+obtenerFecha());
     }
+     public static String obtenerFecha() {
+            LocalDateTime actual = LocalDateTime.now();
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            return actual.format(formato);
+        }
 }
